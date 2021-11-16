8c99fbe627340a5aa36cdf6cc6b3bf82c4032fef
ton-katsu <sakamoto.yoshihisa@gmail.com>
2021-11-16 18:56:09 +0900

    Support some workload resources.

--- scripts/check_k8s_rollout.sh
+++ scripts/check_k8s_rollout.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-# e.g.)  ./check_k8s_rollout.sh -e local -n local-test-container-service -c test-container -w 120
+# e.g.)  ./check_k8s_rollout.sh -e env -n local-test-container-service -c test-container -w 120
+# e.g.)  ./check_k8s_rollout.sh -e env -n local-test-container-service -c test-container -w 120 -r StatefulSets
 #
 export LANG=C
 
@@ -14,29 +15,40 @@ Description:
   $_description
 Options:
   -e env e.g.) "local"
-  -n namespace e.g.) "local-test-container"
-  -c 対象コンテナ e.g.) "test-container"
-  -w 待ち時間) e.g.) "120"
+  -n namespace e.g.) "default"
+  -c 対象コンテナ e.g.) "nginx"
+  -w 待ち時間 e.g.) "120"
+  -r workload resource (optional) | Deployments | ReplicaSet | StatefulSets | DaemonSet | Default is Deployments.
 _EOT_
   exit 2
 }
 
 [ $# -eq 0 ] && usage
-while getopts "he:n:c:w:" opt; do
+while getopts "he:n:c:w:r:" opt; do
     case $opt in
         e) _env=$OPTARG;;
         n) _namespace=$OPTARG;;
         c) _container=$OPTARG;;
         w) _waittime=$OPTARG;;
+        r) _resource=$OPTARG;;
         h) usage;;
     esac
 done
-
 
 [ "x$_env" = "x" ] && echo "[ERROR]: -e option not found." && exit 2
 [ "x$_namespace" = "x" ] && echo "[ERROR]: -n option not found." && exit 2
 [ "x$_container" = "x" ] && echo "[ERROR]: -c option not found." && exit 2
 [ "x$_waittime" = "x" ] && echo "[ERROR]: -w option not found." && exit 2
+_resources="Deployments ReplicaSet StatefulSets DaemonSet"
+_check_result=1
+if [ "x$_resource" = "x" ]; then
+  _resource=Deployments
+else
+  for r in $_resources; do
+    [ "x$r" = "x$_resource" ] && _check_result=0
+  done
+  [ $_check_result = 1 ] && echo "[ERROR]: -r option are $_resources." && exit 2
+fi
 
 echo "[INFO]: Description  : $_description"
 echo "[INFO]: Infomation:"
@@ -44,12 +56,13 @@ echo "[INFO]: env                    : $_env"
 echo "[INFO]: namespace              : $_namespace"
 echo "[INFO]: container              : $_container"
 echo "[INFO]: waittime               : $_waittime"
+echo "[INFO]: resource               : $_resource"
 
 echo "[INFO]: ===> Start!!"
 
 start_time=`date +%s`
 while true ; do
-  kubectl rollout status deployments \
+  kubectl rollout status $_resource \
     --namespace=$_namespace \
     $_container \
     --timeout=1s
